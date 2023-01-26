3a2dca8d3d604c92dadb0e64d3b0ecf3139b5722
Logarithm <kosuke.eng@gmail.com>
2023-01-27 05:42:17 +0900

    Add comment and type hints for lab's members

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ BASE_DIR = "/Users/極微太郎/Documents/CV測定プログラム"
 os.chdir(BASE_DIR)
 
 def setup():
+    """
+        [要約]
+            グラフ保存やCSVファイルを保存するフォルダの生成、
+            USBインスタンスを生成し出力する
+        
+        [引数]
+            なし
+        
+        [return]
+            USBインスタンス
+    """
+    
     IMG_PATH = os.path.join(BASE_DIR, "images")
     RES_PATH = os.path.join(BASE_DIR, "results")
     os.makedirs(IMG_PATH, exist_ok=True)
@@ -26,19 +38,30 @@ def setup():
     
     return target_ins
 
+# config.jsonファイルの読み込み
 config = json.load(open("./config.json", encoding="utf-8"))
-base_info = config["測定情報"]
-graph_info = config["グラフ関連"]
-csv_info = config["csvファイル出力"]
+base_info = config["測定情報"] # 測定情報の読み込み
+graph_info = config["グラフ関連"] # グラフ情報の読み込み
+csv_info = config["csvファイル出力"] # CSVファイル情報の読み込み
 
+# 測定情報から以下の情報を取得
 freq = base_info["周波数[Hz]"]
 start = base_info["開始電圧[V]"]
 end = base_info["終了電圧[V]"]
 ticks = base_info["間隔[V]"]
 times = base_info["測定回数"]
 
-# Error handling
-def error_check():
+def error_check() -> bool:
+    """
+        [要約]
+            configファイル内で不適な指定がされた時のチェックを行う
+        
+        [引数]
+            なし
+        
+        [return]
+            エラーの有無 {bool} -- [エラーがあればFalse, なければTrue]
+    """
     if times < 2:
         print("繰り返し回数は2回以上にしてください")
         return False
@@ -54,14 +77,27 @@ def error_check():
     return True
 
 
-def main():
+def main() -> None:
+    """
+        [要約]
+            modules.pyからクラスを読み込み、configファイルから取得した通りに必要な関数を実行していく
+        
+        [引数]
+            なし
+        
+        [return]
+            なし
+    """
+    
     if error_check() is False:
         print()
         print("３秒後に終了します...")
         time.sleep(3)
         exit()
+    
+    # 測定インスタンスを生成
     mcv = MeasurementCV(setup(), freq, start, end, ticks, times)
-    data = mcv.get_voltage_sweap_data()
+    data = mcv.get_voltage_sweap_data() # 測定結果の４次元Numpy配列
     
     # plot
     if graph_info["プレビューを表示"] or graph_info["グラフを保存"]:
--- modules.py
+++ modules.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from time import strftime
 import tqdm
 import os
+from typing import List, Tuple
 
 BASE_DIR = "/Users/極微太郎/Documents/CV測定プログラム"
 CSV_DIR = os.path.join(BASE_DIR, "results")
@@ -16,34 +17,78 @@ def format_to_date(name: str, last: str):
 
 class MeasurementCV:
     def __init__(self, instance, freq, start, end, ticks, times) -> None:       
+        
+        # 開始電圧が終了電圧より低い場合、以下の処理で反転させる
         if start > end:
             self.ticks = -ticks
         else:
             self.ticks = ticks
         
-        self.ins = instance
-        self.freq = freq
-        self.start = start
-        self.end = end
-        self.times = times
-        self.size = int((self.end - self.start) // self.ticks + 1)
+        self.ins = instance # USB通信のインスタンス
+        self.freq = freq # 周波数
+        self.start = start # 開始電圧
+        self.end = end # 終了電圧
+        self.times = times #　測定回数
+        self.size = int((self.end - self.start) // self.ticks + 1) # データサイズ
 
-    def imp_to_C(self, imp):
+    def imp_to_C(self, imp: float) -> float:
+        """
+        [要約]
+            CV測定器からはインピーダンスと位相が出力される。
+            この関数はインピーダンスからキャパシタンスを変換する
+        
+        [引数]
+            imp {float} -- [インピーダンス]
+        
+        [return]
+            C {float} -- [キャパシタンス]
+        """
+        
         return (2*np.pi*self.freq*float(imp))**-1
 
     def get_machine_name(self) -> str:
+        """
+        [要約]
+            CV測定器の情報を取得し、出力する
+        
+        [引数]
+            なし
+        
+        [return]
+            機器情報 {str} -- [型番や機器名]
+        """
+        
         self.ins.write("*IDN?")
         output = self.ins.read()
         return output
 
     def check_error(self) -> str:
+        """
+        [要約]
+            CV測定器でのエラーを確認し、出力する
+        
+        [引数]
+            なし
+        
+        [return]
+            エラー情報 {str} -- [エラー情報]
+        """
+        
         self.ins.write("*ESR?")
         output = self.ins.read()
         return output
 
-    def base(self, voltage):
+    def base(self, voltage: float) -> Tuple[str, str]:
         """
-        This function only get one output at specific freq and voltage.
+        [要約]
+            任意の電圧の一点を測定し出力する
+            周波数はself.freqを使用する
+        
+        [引数]
+            voltage {float} -- [電圧値]
+        
+        [return]
+            測定結果 {tuple(str, str)} -- [tupleの中身 >> (インピーダンス, 位相)]
         """
 
         # config
@@ -66,14 +111,33 @@ class MeasurementCV:
         return data[0], data[1]
 
     def get_voltage_sweap_data(self):
-        # this dimention means times, (forward, back), datasize, (voltage, C)
+        """
+        [要約]
+            周波数固定で電圧を掃引する測定を行う
+            開始電圧や終了電圧、周波数と測定回数はselfから引用している
+        
+        [引数]
+            なし
+        
+        [return]
+            測定結果の4次元配列 {numpy.ndarray} -- [
+                1次元 -> 測定回数 (平均値を格納するため+1している)
+                2次元 -> 行きと帰り
+                3次元 -> データサイズ (0V ~ 1Vで0.1Vずつなら10となる)
+                4次元 -> 電圧とキャパシタンス
+            ]
+        """
+        
+        # 結果用のNumpy配列生成し、0で初期化
         result = np.zeros((self.times + 1, 2, self.size, 2))
 
+        # 測定回数のループ
         for t in range(self.times):
             print("-"*100)
             print(f"{t+1}回目 測定開始")
             print("-"*100)
-            # forward
+            
+            # 行きの測定
             print("start >>> end 測定中...")
             for idx, v in enumerate(tqdm.tqdm(np.arange(self.start, self.end, self.ticks))):
                 imp, phase = self.base(v)
@@ -81,7 +145,7 @@ class MeasurementCV:
                 result[t, 0, idx, 1] = self.imp_to_C(imp)
             print()
             
-            # back
+            # 帰りの測定
             print("end >>> start 測定中...")
             for idx, v in enumerate(tqdm.tqdm(np.arange(self.end, self.start, -self.ticks))):
                 imp, phase = self.base(v)
@@ -92,58 +156,117 @@ class MeasurementCV:
             print(f"{t+1}回目 測定終了")
             print()
 
-        # calculate average
-        # forward
+        # 以下、平均値を計算し取得する
+        
+        # 行きの平均値
         for idx, v in enumerate(np.arange(self.start, self.end, self.ticks)):
             result[-1, 0, idx, 0] = v
             result[-1, 0, idx, 1] = np.average(result[:-1, 0, idx, 1])
         
-        # back
+        # 帰りの平均値
         for idx, v in enumerate(np.arange(self.end, self.start, -self.ticks)):
             result[-1, 1, idx, 0] = v
             result[-1, 1, idx, 1] = np.average(result[:-1, 1, idx, 1])
 
         return result
 
-
 class PlotData:
 
     def __init__(self, data, linewidth, linestyle, is_grid, is_forward, is_back) -> None:
-        self.data = data
-        self.times = data.shape[0] - 1
-        self.linewidth = linewidth
-        self.linestyle = linestyle
-        self.is_grid = is_grid
-        self.is_forward = is_forward
-        self.is_back = is_back
-        self.fig = plt.figure(dpi=100)
+        self.data = data # Numpy配列を取得
+        self.times = data.shape[0] - 1 # 測定回数を取得 (平均値の分を引く)
+        self.linewidth = linewidth # グラフの線の太さ
+        self.linestyle = linestyle # グラフの線スタイル
+        self.is_grid = is_grid # グリッド表示
+        self.is_forward = is_forward # 行きをプロットするかどうか
+        self.is_back = is_back # 帰りをプロットするかどうか
+        self.fig = plt.figure(dpi=100) # フィギュアの生成
     
-    def figure_setup(self, y_range):
+    def figure_setup(self, y_range: Tuple[float, float]) -> None:
+        """
+        [要約]
+            グラフ表示のmax, minなど指定された時に実行する関数
+        
+        [引数]
+            y_range {tuple(float, float)} -- [ tuple(max, min) ]
+                    
+        [return]
+            なし
+        """
         y_min, y_max = y_range
         plt.ylim(y_min, y_max)
         
     
-    def plot_avg_else(self):
+    def plot_avg_else(self) -> None:
+        """
+        [要約]
+            測定回数分の結果をプロットする
+        
+        [引数]
+            なし
+        
+        [return]
+            なし
+        """
+        
+        # 測定回数のループ
         for t in range(self.times):
+            # もし行きのプロットをするなら、プロットする
             if self.is_forward:
                 plt.plot(self.data[t, 0, :, 0], self.data[t, 0, :, 1], label=f"foward {t+1}times", lw=self.linewidth, linestyle=self.linestyle)
             
+            # もし帰りのプロットをするなら、プロットする
             if self.is_back:
                 plt.plot(self.data[t, 1, :, 0], self.data[t, 1, :, 1], label=f"back {t+1}times", lw=self.linewidth, linestyle=self.linestyle)
 
-    def plot_avg(self):
+    def plot_avg(self) -> None:
+        """
+        [要約]
+            平均値をプロットする
+        
+        [引数]
+            なし
+        
+        [return]
+            なし
+        """
+        
+        # もし行きのプロットをするなら、プロットする
         if self.is_forward:
             plt.plot(self.data[-1, 0, :, 0], self.data[-1, 0, :, 1], label=f"foward average", lw=self.linewidth, linestyle=self.linestyle)
-            
+        
+        # もし帰りのプロットをするなら、プロットする
         if self.is_back:
             plt.plot(self.data[-1, 1, :, 0], self.data[-1, 1, :, 1], label=f"back average", lw=self.linewidth, linestyle=self.linestyle)
 
-    def preview(self):
+    def preview(self) -> None:
+        """
+        [要約]
+            PC画面上にプロット結果を表示する
+        
+        [引数]
+            なし
+        
+        [return]
+            なし
+        """
         plt.legend()
         plt.grid(self.is_grid)
         plt.show()
     
-    def save_figure(self, img_name, is_stansparent):
+    def save_figure(self, img_name: str, is_stansparent: bool) -> None:
+        """
+        [要約]
+            プロット結果を保存する
+        
+        [引数]
+            img_name {str} -- [画像保存名で指定がなければ現在時刻に変換する]
+            is_transparent {bool} -- [保存する画像の背景を透過させるかどうか]
+        
+        [return]
+            なし
+        """
+        
         plt.legend()
         plt.grid(self.is_grid)
         img_name = format_to_date(img_name, ".png")
@@ -153,15 +276,26 @@ class PlotData:
 class OutputCSV:
     
     def __init__(self, data, dir_name) -> None:
-        self.data = data
-        self.times = data.shape[0] - 1
-        self.dir_name = format_to_date(dir_name, "")
+        self.data = data # Numpy配列を取得
+        self.times = data.shape[0] - 1 # 測定回数を取得 (平均値の分を引く)
+        self.dir_name = format_to_date(dir_name, "") # 複数のCSVファイルを保存するフォルダの名前
     
-    def output(self):
+    def output(self) -> None:
+        """
+        [要約]
+            CSVファイルをディレクトリに保存していく
+        
+        [引数]
+            なし
+        
+        [return]
+            なし
+        """
+        
         SAVE_DIR = os.path.join(CSV_DIR, self.dir_name)
         os.makedirs(SAVE_DIR, exist_ok=True)
         
-        # except average
+        # 測定回数分をCSVファイルへ変換・出力
         for t in range(self.times):
             for i in range(2):
                 vector = "行き" if i == 0 else "帰り"
@@ -169,11 +303,9 @@ class OutputCSV:
                 tmp = pd.DataFrame(self.data[t, i])
                 tmp.to_csv(file_name)
         
-        # average
+        # 平均値をCSVファイルへ変換・出力
         for i in range(2):
             vector = "行き" if i == 0 else "帰り"
             file_name = os.path.join(SAVE_DIR, f"平均値_{vector}.csv")
             tmp = pd.DataFrame(self.data[-1, i])
             tmp.to_csv(file_name)
-        
-        return
